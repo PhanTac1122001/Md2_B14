7606dd213b8cb24e477354dbd34341973c8d3b11
PhanTac1122001 <tintert1703@gmail.com>
2024-06-27 08:00:26 +0700

    MD2_B14

---
+++ src/baitapvenha/bai2/TimeExample.java
@@ -0,0 +1,10 @@
+package baitapvenha.bai2;
+
+import java.time.LocalTime;
+
+public class TimeExample {
+    public static void main(String[] args) {
+        LocalTime currentTime = LocalTime.now();
+        System.out.println(currentTime);
+    }
+}
---
+++ src/luyentap/b1/Shape.java
@@ -0,0 +1,7 @@
+package luyentap.b1;
+
+public interface Shape {
+    default double area(){
+        return 0.0;
+    }
+}
